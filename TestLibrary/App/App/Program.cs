934a6010928577a0139b256078b756b8443ec48e
Ryuunooo <91253167+Ryuunooo@users.noreply.github.com>
2022-11-14 13:24:46 +0300

    Add files via upload

---
+++ TestLibrary/App/App/Program.cs
@@ -0,0 +1,4 @@
+﻿using System;
+using CoreLib;
+Console.WriteLine(utils.GetStudNumber(2022, 195, "Гашев Вячеслав Иванович"));
+Console.ReadKey();
---
+++ TestLibrary/CoreLib/CoreLib/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLib
+{
+    public class utils
+    {
+        public static string GetStudNumber(int year, int group, string fio)
+        {
+            var fullName = fio.Split(' ');
+            return $"{year.ToString()}_{group.ToString()}_{fullName[0]} {fullName[1].Substring(0, 1)}.{fullName[2].Substring(0, 1)}.";
+        }
+    }
+}
+
